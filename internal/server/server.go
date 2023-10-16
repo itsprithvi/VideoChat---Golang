594693259bb2770e00d5589def01d633b5a8be84
itsprithvi <prithvi.myk@gmail.com>
2023-10-16 18:13:35 +0530

    impl basic skeliton of room for chat and video

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,14 @@ import (
 	"flag"
 	"os"
 	"time"
+
+	"github.com/gofiber/fiber/middleware/logger"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
+	"golang.org/x/net/websocket"
 )
 
 
@@ -20,11 +28,18 @@ func Run() error {
 	if *addr == ":" {
 		*addr = ":8080"
 	}
+
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views:engine})
+	app.Use(logger.New())
+	app.Use(cors.New())
 	
 	app.Get("/", handers.Welcome)
 	app.Get("/room/create", handerls.RoomCreate)
 	app.Get("/room/:uuid", handlers.Room)
-	app.Get("/room/:uuid/websocket", )
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebsocket, websocket.Config{
+		HandshakeTimeout: 10*time.Second, 
+	}))
 	app.Get("/room/:uuid/chat", handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
 	app.Get("/room/uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
